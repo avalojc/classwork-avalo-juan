aabf6a4321ea3613dab3641c5202338b8857f031
JC <avalojc@gmail.com>
2020-11-03 10:00:33 -0500

    spring initialize

---
+++ battleship/src/test/java/com/talentpath/battleship/BattleshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentpath.battleship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
